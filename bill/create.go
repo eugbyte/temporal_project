fb6b5ca478bd4aa82298a0e895f8c0bf6bf186f2
Eugene <eugenetham1994@gmail.com>
2023-11-26 19:59:48 +0800

    chore: rename mods (#4)
    * chore: rename mods
    
    * chore: add ci

--- bill/create.go
+++ bill/create.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	db "encore.app/internal/db/bill"
-	workflows "encore.app/internal/temporal/bill/workflow"
+	"encore.app/internal/temporal/bill/workflows"
 	"go.temporal.io/sdk/client"
 )
 
